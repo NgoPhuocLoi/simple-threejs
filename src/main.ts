499dd1c04ba31e045f581d74f09cd48b471bedde
NgoPhuocLoi <phuocloi11223@gmail.com>
2024-07-21 18:03:26 +0700

    Playing around with Renderer

--- src/main.ts
+++ src/main.ts
@@ -1,32 +1,19 @@
 import "./style.css";
 import * as THREE from "three";
-import { OrbitControls } from "three/examples/jsm/Addons.js";
-import Stats from "three/examples/jsm/libs/stats.module.js";
-import { GUI } from "dat.gui";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 
-const sceneA = new THREE.Scene();
-const sceneB = new THREE.Scene();
-const sceneC = new THREE.Scene();
-sceneA.background = new THREE.Color(0x00ff00);
-sceneB.background = new THREE.TextureLoader().load(
-  "https://sbcode.net/img/grid.png",
-);
-sceneC.background = new THREE.CubeTextureLoader()
-  .setPath("https://sbcode.net/img/")
-  .load(["px.png", "nx.png", "py.png", "ny.png", "pz.png", "nz.png"]);
-// scene.backgroundBlurriness = 0.5;
+const scene = new THREE.Scene();
 
-const camera = new THREE.PerspectiveCamera(
-  75,
-  window.innerWidth / window.innerHeight,
-  0.1,
-  1000,
-);
+const camera = new THREE.PerspectiveCamera(75, 2, 0.1, 1000);
 camera.position.z = 1.5;
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+const canvas = document.getElementById("canvas");
+
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas as HTMLCanvasElement,
+  antialias: true,
+});
+renderer.setSize(400, 200);
 
 window.addEventListener("resize", () => {
   camera.aspect = window.innerWidth / window.innerHeight;
@@ -40,40 +27,12 @@ const geometry = new THREE.BoxGeometry();
 const material = new THREE.MeshNormalMaterial({ wireframe: true });
 
 const cube = new THREE.Mesh(geometry, material);
-sceneA.add(cube);
-
-const stats = new Stats();
-document.body.appendChild(stats.dom);
-
-const gui = new GUI();
-let activeScene = sceneA;
-const renderScene = {
-  sceneA: () => {
-    activeScene = sceneA;
-  },
-  sceneB: () => {
-    activeScene = sceneB;
-  },
-  sceneC: () => {
-    activeScene = sceneC;
-  },
-};
-
-gui.add(renderScene, "sceneA").name("Scene A");
-gui.add(renderScene, "sceneB").name("Scene B");
-gui.add(renderScene, "sceneC").name("Scene C");
-
-gui.open();
+scene.add(cube);
 
 function animate() {
   requestAnimationFrame(animate);
 
-  // cube.rotation.x += 0.01;
-  // cube.rotation.y += 0.01;
-
-  renderer.render(activeScene, camera);
-
-  stats.update();
+  renderer.render(scene, camera);
 }
 
 animate();
